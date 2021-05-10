c7493d6fa5f951297611c2cc08c060d12697fe01
jrm2087 <jrm2087@gmail.com>
2021-05-10 15:27:07 -0500

    Generacion de ruta home y about con plantilla

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = """
+<h1>MyApp</h1>
+<ul>
+   <li> <a href="/">Portada</a> </li>
+   <li> <a href="/about-me/">Acerca de</a> </li>
+</ul>
+"""
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Mi web</h1> <h2>jrm2087</h2>')
+    return HttpResponse(html_base + '<h2>Portada</h2>')
+
+def about(request):
+     return HttpResponse(html_base + '<h2>Acerca de</h2>')
