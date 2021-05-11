82766fe21650bb8f45739ae32d293710cd3158ef
jrm2087 <jrm2087@gmail.com>
2021-05-11 10:05:21 -0500

    ajute vistas

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return render(request,'core/home.html')
+    return render(request, 'core/home.html')
+
 
 def about(request):
-    return render(request,'core/about.html')
+    return render(request, 'core/about.html')
+
 
 def portfolio(request):
-    return render(request,'core/portfolio.html')
+    return render(request, 'core/portfolio.html')
+
 
 def contact(request):
-    return render(request,'core/contact.html')
+    return render(request, 'core/contact.html')
