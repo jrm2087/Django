e0cf31ffe244a55f929b716263ce964a67d67aa6
José <63377749+jrm2087@users.noreply.github.com>
2021-07-11 18:52:47 -0500

    crud vistas page

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Page
 
 
@@ -9,3 +11,23 @@ class PagesListView(ListView):
 
 class PageDetailView(DetailView):
     model = Page
+
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
+
+
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
