8172f1965f4d03937fb2f0f5cc57a58d21ebb784
José <63377749+jrm2087@users.noreply.github.com>
2021-07-15 20:59:46 -0500

    mixin para validar permisos a las rutas

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -2,8 +2,20 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
+
+
+class StaffRequiredMixin(object):
+    """
+    Este Mixim obliga que el usuario este en el staff
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 
 class PagesListView(ListView):
@@ -14,13 +26,13 @@ class PageDetailView(DetailView):
     model = Page
 
 
-class PageCreate(CreateView):
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
 
-class PageUpdate(UpdateView):
+class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = '_update_form'
@@ -29,6 +41,6 @@ class PageUpdate(UpdateView):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
 
-class PageDelete(DeleteView):
+class PageDelete(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
