cd94ee4657e14b97352006682fbef415e7ba73cc
José <63377749+jrm2087@users.noreply.github.com>
2021-07-19 16:19:00 -0500

    pagina de perfil

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,7 +1,11 @@
-from .forms import UserCreationFormWithEmail
+from .forms import UserCreationFormWithEmail, ProfileForm
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
+from .models import Profile
 
 
 class SignUpView(CreateView):
@@ -24,3 +28,15 @@ class SignUpView(CreateView):
         form.fields['password2'].widget = forms.PasswordInput(
             attrs={'class': 'form-control mb-2', 'placeholder': 'Repetir contraseña'})
         return form
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdate(UpdateView):
+    form_class = ProfileForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_form.html'
+
+    def get_object(self):
+        profile, created = Profile.objects.get_or_create(
+            user=self.request.user)
+        return profile
--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Auth redirects
-LOGIN_REDIRECT_URL = 'pages:pages'
+# LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
 
 # Emails
@@ -139,3 +139,7 @@ if DEBUG:
 else:
     # configurar cuenta real
     pass
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
