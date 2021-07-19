224ceeef86b49c38d726c20d36a3b79c9daa934f
José <63377749+jrm2087@users.noreply.github.com>
2021-07-18 19:45:22 -0500

    app registration

--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -20,5 +20,7 @@ from pages.urls import pages_patterns
 urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    # path de auth
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
