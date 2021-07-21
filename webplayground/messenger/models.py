ea14c2df103e70eeebf56b791adb597788d2f0a5
José <63377749+jrm2087@users.noreply.github.com>
2021-07-20 21:05:39 -0500

    refactorizacion codigo test

--- webplayground/messenger/models.py
+++ webplayground/messenger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
 
 
 class Message(models.Model):
@@ -14,3 +15,24 @@ class Message(models.Model):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop('instance', None)
+    action = kwargs.pop('action', None)
+    pk_set = kwargs.pop('pk_set', None)
+    print(instance, action, pk_set)
+
+    false_pk_set = set()
+    if action is 'pre_add':
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if(msg.user not in instance.users.all()):
+                #print(f'{msg.user} no forma parte del hilo.')
+                false_pk_set.add(msg_pk)
+
+    # filtrar los mensajes q no son del usuario
+    pk_set.difference_update(false_pk_set)
+
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
