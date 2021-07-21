ccc75cae8f21aff480fd1fc285c69167b0f7d6de
José <63377749+jrm2087@users.noreply.github.com>
2021-07-20 19:35:59 -0500

    app messenger

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'registration',
-    'profiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'core',
-    'pages.apps.PagesConfig'
+    'messenger',
+    'pages.apps.PagesConfig',
+    'profiles',
 ]
 
 MIDDLEWARE = [
