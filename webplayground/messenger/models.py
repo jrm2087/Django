62a5f67e371777b695654d1b7bfcbdcb376f3dd7
José <63377749+jrm2087@users.noreply.github.com>
2021-07-22 23:15:15 -0500

    implementacion js mensajes en perfiles

--- webplayground/messenger/models.py
+++ webplayground/messenger/models.py
@@ -30,8 +30,12 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
 
     objects = ThreadManager()
+
+    class Meta:
+        ordering = ['-updated']
 
 
 def messages_changed(sender, **kwargs):
@@ -50,6 +54,9 @@ def messages_changed(sender, **kwargs):
 
     # filtrar los mensajes q no son del usuario
     pk_set.difference_update(false_pk_set)
+
+    # forzar la actualizacion haciendo save
+    instance.save()
 
 
 m2m_changed.connect(messages_changed, sender=Thread.messages.through)
